e0ac192d0c71474bca31bdc4e9e21680177959c6
youssef-alaa-elneel <youssefalaaelneel@gmail.com>
2026-04-06 01:40:37 +0200

    delete yassa's files out side project

--- src/components/layout/GroupDetailsSidebar.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export const GroupDetailsSidebar = () => {
-  return (
-    <aside className="w-64 h-full border-l border-gray-200 dark:border-gray-700 p-4">
-      <h2 className="text-lg font-bold mb-4">Group Details</h2>
-    </aside>
-  );
-};
