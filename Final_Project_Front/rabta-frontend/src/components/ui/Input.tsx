9b3a07d496f971e9bef8e3bdcbe39a53bc7681d3
youssef-alaa-elneel <youssefalaaelneel@gmail.com>
2026-04-07 01:14:06 +0200

    start Add Form validations and Auth error toasts. Task but unfinished yet

--- Final_Project_Front/rabta-frontend/src/components/ui/Input.tsx
+++ Final_Project_Front/rabta-frontend/src/components/ui/Input.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
+import React, { forwardRef } from "react";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  error?: string; // دي عشان لو فيه إيرور نعرضه تحت الحقل
 }
 
 export const Input = ({ label, id, ...props }: InputProps) => {
@@ -10,10 +10,10 @@ export const Input = ({ label, id, ...props }: InputProps) => {
       <label htmlFor={id} className="block text-sm font-medium mb-2">
         {label}
       </label>
-      <input 
+      <input
         id={id}
         className="w-full px-4 py-3 bg-transparent border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:border-[#7C3AED] dark:focus:border-[#8B5CF6] focus:ring-1 focus:ring-[#7C3AED] dark:focus:ring-[#8B5CF6] transition-all placeholder-gray-400 dark:placeholder-gray-500"
-        {...props} 
+        {...props}
       />
     </div>
   );
--- Final_Project_Front/rabta-frontend/src/main.tsx
+++ Final_Project_Front/rabta-frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
       <App />
     </BrowserRouter>
   </StrictMode>,
